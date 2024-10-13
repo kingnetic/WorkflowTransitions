82ea4d2028f8fd0b3da1709ee9efdf83a2e42be7
Marvin <marangoes@gmail.com>
2024-10-13 14:34:54 -0600

    Configure React app for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/vite.svg', // Path to your source file
+          dest: '', // Destination folder in the build output
+        },
+      ],
+    }),
+    
+  ],
 });
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
